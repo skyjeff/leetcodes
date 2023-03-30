713c9667f8022792a6c2ebcd3be5ebed31b2ee5a
chenhao <505042464@qq.com>
2023-03-30 08:40:12 +0800

    feat: 670,713,729

---
+++ 670.最大交换.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=670 lang=python3
+#
+# [670] 最大交换
+#
+
+# @lc code=start
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        max_ = max(num)
+
+        for i in range(len(num)):
+            
+# @lc code=end
+
---
+++ 713.乘积小于-k-的子数组.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=713 lang=python3
+#
+# [713] 乘积小于 K 的子数组
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        l= 0
+        sum = 1
+        res = 0
+        for j in range(n):
+            sum *= nums[j]
+            while sum >= k and l <= j:
+                sum //= nums[l]
+                l+=1
+            res += j - l + 1
+        return res
+# @lc code=end
+Solution().numSubarrayProductLessThanK([10,5,2,6],
+100)
+
